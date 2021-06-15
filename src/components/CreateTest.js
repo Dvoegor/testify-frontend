1df4c24036cbe74b196cbcb85bdb7b2a95e4d4ac
Grigorii Dvoeglazov <dvoegor@gmail.com>
2021-06-15 16:36:30 +0400

    0.2 - fixed table and list page

--- src/components/CreateTest.js
+++ src/components/CreateTest.js
@@ -5,8 +5,10 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import Table from "./Table";
+import List from "./List";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,27 +27,34 @@ function getSteps() {
   return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
 }
 
-function getStepContent(stepIndex) {
-  switch (stepIndex) {
-    case 0:
-      return (
-          <div>
-            <Table/>
-          </div>
-      );
-    case 1:
-      return (
-          <div>
-          </div>
-      );
-    case 2:
-      return 'This is the bit I really care about!';
-    default:
-      return 'Unknown stepIndex';
-  }
-}
-
 export default function HorizontalLabelPositionBelowStepper() {
+  const [idArr, setIdArr] = React.useState([]);
+
+  const sendDataToParent = (index) => { // the callback. Use a better name
+    setIdArr(index);
+  };
+
+  function getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return (
+            <div>
+              <Table sendDataToParent={sendDataToParent}/>
+            </div>
+        );
+      case 1:
+        return (
+            <div>
+              <List idArr={idArr} />
+            </div>
+        );
+      case 2:
+        return 'This is the bit I really care about!';
+      default:
+        return 'Unknown stepIndex';
+    }
+  }
+
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
@@ -80,6 +89,7 @@ export default function HorizontalLabelPositionBelowStepper() {
           ) : (
               <div>
                 <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                <Grid container spacing={0} alignItems="center" alignContent="center" justify="center" style={{marginBottom: 20, marginTop: 10}}>
                 <div>
                   <Button
                       disabled={activeStep === 0}
@@ -92,6 +102,7 @@ export default function HorizontalLabelPositionBelowStepper() {
                     {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                   </Button>
                 </div>
+                </Grid>
               </div>
           )}
         </div>
